15a2fbe8acb8f9ce279ad3ebbd94aef91a96acaf
BuildTools <unconfigured@null.spigotmc.org>
2018-11-11 17:05:12 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,28 +21,66 @@ var cards = [
 }
 ];
 
+var scores = [];
+var score = 0;
+
+
+var scoreCalculator = function(){	
+	score++;
+
+	var scoreElement = document.createElement('span');
+	var scoreDisplay = document.createTextNode("I");
+	scoreElement.appendChild(scoreDisplay);
+	document.getElementById('scoreNum').appendChild(scoreElement);
+}
+
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++)
+	{
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
-	if (cardsInPlay.length === 2)
+		
+	if (cardsInPlay.length === 2 )
 	{
 		if (cardsInPlay[0] === cardsInPlay[1])
+		{
 			alert("You found a match!");
+			scoreCalculator();
+		}
 		else
 			alert("Sorry, try again!");
 	}
 }
 
-var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	cardsInPlay.push(cards[cardId].rank);
+
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
 
+var clearBoard = function(){
+	for(var i = 0; i < cards.length; i++)
+	{
+		document.querySelectorAll('img')[i].setAttribute('src', 'images/back.png');
+	}
 
+	cardsInPlay.length = 0;
+}
+
+document.getElementById('redo').addEventListener('click', clearBoard);
 
